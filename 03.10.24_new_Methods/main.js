b1004e0ed67b4b5cdf4b38d0e0f3037c39d6a897
Nathan Kilinski <nathkilin@gmail.com>
2024-10-04 17:26:44 +0300

    still studing

--- 03.10.24_new_Methods/main.js
+++ 03.10.24_new_Methods/main.js
@@ -24,15 +24,38 @@
 /////////////////////////////////////////////////////
 
 // filter
-const people = [
-    { name: 'Alice', age: 22 },
-    { name: 'Bob', age: 17 },
-    { name: 'Anna', age: 19 },
-    { name: 'Mike', age: 23 }
-  ];
-let filtered_People = people.filter(function(person){
-    return person.name[0] === 'A' && person.age >18
-})
-console.log(filtered_People)
+// const people = [
+//     { name: 'Alice', age: 22 },
+//     { name: 'Bob', age: 17 },
+//     { name: 'Anna', age: 19 },
+//     { name: 'Mike', age: 23 }
+//   ];
+// let filtered_People = people.filter(function(person){
+//     return person.name[0] === 'A' && person.age >18
+// })
+// console.log(filtered_People)
 
 ////////////////////////////////////////////////////////
+
+//reduce
+// const fruits = ['apple', 'banana']
+// const letterCount = fruits.reduce(function(counts, word) {
+//     word.split('').forEach(function(letter) {
+//       counts[letter] = (counts[letter] || 0) + 1;
+//     });
+//     return counts;
+//   }, {});
+//   console.log(letterCount);
+
+
+////////////////////////////////////////////////////////
+
+// some & every
+const passwords = ['1234', 'abc', '123abc', 'longpassword123']
+
+const hasValidPassword = passwords.some(function(password){
+    const hasLetters = /[a-zA-z]/.test(password)
+    const hasNumbers = /\d/.test(password)
+    return password.length > 8 && hasLetters && hasNumbers
+})
+console.log(hasValidPassword);
